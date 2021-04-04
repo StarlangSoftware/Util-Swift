e5a183c8da6818b35a51455b08e7907d9af67044
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-04-04 11:26:38 +0300

    Added wrapper function for reading text files.

---
+++ Sources/Util/FileUtils.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by Olcay Taner YILDIZ on 4.04.2021.
+//
+
+import Foundation
+
+public class FileUtils{
+    
+    public static func getLines(fileName: String) -> [String.SubSequence]?{
+        let thisSourceFile = URL(fileURLWithPath: #file)
+        let thisDirectory = thisSourceFile.deletingLastPathComponent()
+        let url = thisDirectory.appendingPathComponent(fileName)
+        do{
+            let fileContent = try String(contentsOf: url, encoding: .utf8)
+            return fileContent.split(whereSeparator: \.isNewline)
+        } catch {
+            return nil
+        }
+    }
+
+    public static func getItems(fileName: String) -> [[String]]?{
+        var items : [[String]] = []
+        let thisSourceFile = URL(fileURLWithPath: #file)
+        let thisDirectory = thisSourceFile.deletingLastPathComponent()
+        let url = thisDirectory.appendingPathComponent(fileName)
+        do{
+            let fileContent = try String(contentsOf: url, encoding: .utf8)
+            let lines : [String] = fileContent.split(whereSeparator: \.isNewline).map(String.init)
+            for line in lines{
+                let wordList : [String] = line.split(separator: " ").map(String.init)
+                items.append(wordList)
+            }
+            return items
+        } catch {
+            return nil
+        }
+    }
+}
---
+++ Tests/UtilTests/FileUtilsTest.swift
@@ -0,0 +1,24 @@
+import XCTest
+@testable import Util
+
+final class FileUtilsTest: XCTestCase {
+
+    func testFile() {
+        let list = FileUtils.getLines(fileName: "deneme.txt")
+        XCTAssertNotNil(list)
+        XCTAssertEqual(2, list?.count)
+    }
+
+    func testItems() {
+        let list = FileUtils.getItems(fileName: "deneme.txt")
+        XCTAssertNotNil(list)
+        XCTAssertEqual(2, list?.count)
+        XCTAssertEqual(2, list?[0].count)
+        XCTAssertEqual(3, list?[1].count)
+    }
+
+    static var allTests = [
+        ("testExample1", testFile),
+        ("testExample2", testItems),
+    ]
+}
