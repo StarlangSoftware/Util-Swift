579286e1e5250ab0852794c503f27bbf2a6f52fb
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-16 14:40:33 +0300

    Added RectAngle class.

---
+++ Sources/Util/RectAngle.swift
@@ -0,0 +1,44 @@
+//
+//  File.swift
+//  
+//
+//  Created by Olcay Taner YILDIZ on 16.04.2022.
+//
+
+import Foundation
+
+public class RectAngle{
+    
+    private var x: Int
+    private var y: Int
+    private var width: Int
+    private var height: Int
+    
+    init(x: Int, y: Int, width: Int, height: Int){
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    }
+    
+    public func contains(x: Int, y: Int) -> Bool{
+        return x >= self.x && x <= self.x + width &&
+        y >= self.y && y <= self.y + height
+    }
+    
+    public func getX() -> Int{
+        return x
+    }
+    
+    public func getY() -> Int{
+        return y
+    }
+    
+    public func getWidth() -> Int{
+        return width
+    }
+    
+    public func getHeight() -> Int{
+        return height
+    }
+}
